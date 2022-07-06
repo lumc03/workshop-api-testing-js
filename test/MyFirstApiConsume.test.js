349f951390c8f1b3d135ba4fefc493a49234be71
lumc03 <luisa.marinc23@gmail.com>
2022-07-06 15:43:19 -0500

    Veriy data object

--- test/MyFirstApiConsume.test.js
+++ test/MyFirstApiConsume.test.js
@@ -3,104 +3,89 @@ const { expect } = require('chai');
 const { StatusCodes } = require('http-status-codes');
 
 describe('First Api Tests', () => {
+  // Get
+  it('Consume GET Service', async () => {
+    const response = await axios.get('https://httpbin.org/get');
 
-    //Get
-    it('Consume GET Service', async () => {
-        const response = await axios.get('https://httpbin.org/get');
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data).to.have.property('origin');
+  });
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data).to.have.property('origin');
-    })
+  // Get with Parameters
+  it('Consume GET Service with Query Parameters', async () => {
+    config = {
+      id: 3
+    };
 
+    const response = await axios.get('https://httpbin.org/get', config);
 
-    //Get with Parameters
-    it('Consume GET Service with Query Parameters', async () => {
-        config = {
-            id: 3,
-        };
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.config.id).to.equal(3);
+  });
 
-        const response = await axios.get('https://httpbin.org/get', config);
+  // Head
+  it('Consume Head Service', async () => {
+    config = {
+      id: 3
+    };
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.config.id).to.equal(3);
-    });
+    const response = await axios.head('https://httpbin.org', config);
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.data).to.empty;
+  });
 
-    //Head 
-    it('Consume Head Service', async () => {
+  // Post
+  it('Consume Post Service', async () => {
+    config = {
+      id: 3
+    };
+    data = {
+      name: 'luisa',
+      age: 22
 
-        config = {
-            id: 3,
-        };
+    };
 
-        const response = await axios.head('https://httpbin.org', config);
+    const response = await axios.post('https://httpbin.org/post', data, config);
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.config.id).to.equal(3);
+    expect(response.data.json).to.eql(data);
+  });
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.data).to.empty;
-    });
+  it('Consume Patch Service', async () => {
+    config = {
+      id: 3
+    };
 
-    //Post
-    it('Consume Post Service', async () => {
+    data = {
+      name: 'sofia'
+    };
 
-        config = {
-            id: 3
-        }
-        data = {
-            name: 'luisa',
-            age: 22
+    const response = await axios.patch('https://httpbin.org/patch', data, config);
 
-        };
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.config.id).to.equal(3);
+    expect(response.data.json).to.eql(data);
+  });
 
-        const response = await axios.post('https://httpbin.org/post', data, config);
+  it('Consume Put Service', async () => {
+    data = {
+      name: 'maria',
+      age: 20
+    };
 
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.config.id).to.equal(3);
+    const response = await axios.put('https://httpbin.org/put', data, { id: 3 });
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.config.id).to.equal(3);
+    expect(response.data.json).to.eql(data);
+  });
 
+  it('Consume Delete Service', async () => {
+    config = {
+      id: 3
+    };
 
-    });
-
-    it('Consume Patch Service', async () => {
-
-        config = {
-            id: 3,
-        }
-
-        data = {
-            name: 'sofia',
-        };
-
-        const response = await axios.patch('https://httpbin.org/patch', data, config);
-
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.config.id).to.equal(3);
-
-    });
-
-    it('Consume Put Service', async () => {
-
-        data = {
-            name: 'maria',
-            age: 20
-        }
-
-
-        const response = await axios.put('https://httpbin.org/put', data, { id: 3 });
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.config.id).to.equal(3);
-
-    });
-
-
-
-    it('Consume Delete Service', async () => {
-
-        config = {
-            id: 3
-        }
-
-
-        const response = await axios.delete('https://httpbin.org/delete', config);
-        expect(response.status).to.equal(StatusCodes.OK);
-        expect(response.config.id).to.equal(3);
-
-    });
+    const response = await axios.delete('https://httpbin.org/delete', config);
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.config.id).to.equal(3);
+  });
 });
