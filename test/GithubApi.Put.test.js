38144576919231d79e9a850bd86e9b275b386278
lumc03 <107214750+lumc03@users.noreply.github.com>
2022-07-15 09:32:56 -0500

    #9 consume put method (#6)
    * #9 consume put method
    
    * Test to verify still following user
    
    * Test to verify is still following the user

---
+++ test/GithubApi.Put.test.js
@@ -0,0 +1,45 @@
+const { StatusCodes } = require('http-status-codes');
+const { expect } = require('chai');
+const axios = require('axios');
+
+const instance = axios.create({
+  headers: {
+    Authorization: `token ${process.env.ACCESS_TOKEN}`
+  }
+});
+const urlBase = 'https://api.github.com/user/following';
+let userFollowed;
+
+describe('Consume Put Method', () => {
+  it('Follow a user service', async () => {
+    const response = await instance.put(`${urlBase}/aperdomob`);
+
+    expect(response.status).to.equal(StatusCodes.NO_CONTENT);
+    expect(response.data).to.equal('');
+  });
+
+  it('Following Check', async () => {
+    const response = await instance.get(`${urlBase}`);
+
+    userFollowed = response.data.find((i) => i.login === 'aperdomob');
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(userFollowed.login).to.equal('aperdomob');
+  });
+
+  it('Follow a user again', async () => {
+    const response = await instance.put(`${urlBase}/aperdomob`);
+
+    expect(response.status).to.equal(StatusCodes.NO_CONTENT);
+    expect(response.data).to.equal('');
+  });
+
+  it('Still following user', async () => {
+    const response = await instance.get(`${urlBase}`);
+
+    userFollowed = response.data.find((i) => i.login === 'aperdomob');
+
+    expect(response.status).to.equal(StatusCodes.OK);
+    expect(userFollowed.login).to.equal('aperdomob');
+  });
+});
